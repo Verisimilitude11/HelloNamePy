6b39316c2d060b9851b7a85d93c7ed5dbbff72c8
Verisimilitude <acharyapiyush1@outlook.com>
2022-01-21 16:58:25 -0800

    Add files via upload

---
+++ HelloNamePy.py
@@ -0,0 +1,8 @@
+#Print Hello World
+print("Hello World!")
+#Get input for "What is your name" and assign it to the variable name
+
+#Now watch the next line of code perform magic!!
+print("Mwahaha! I know your name now, " + name + "!")
+#Also, try to add more information on the robot asking questions in the other code assignment
+
